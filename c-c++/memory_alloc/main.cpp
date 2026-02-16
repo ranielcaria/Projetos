5a45fb7f180af2c08f2a0a3e5b9d1b3d158ca524
Rániel <83562541+ranielcaria@users.noreply.github.com>
2026-02-16 16:55:46 -0300

    Refactor memory allocation and display functions
    Changes:
    
    -> On displayList, the while goes through the loop and the last Node will always point to the nullptr (0 or 0x0) indicating the end of the line.
    
    -> freeMemory added with a temporary variable 'nextNode' to hold the next node before the present one gets full and start a memory leak.

--- c-c++/memory_alloc/main.cpp
+++ c-c++/memory_alloc/main.cpp
@@ -13,36 +13,50 @@ void addNode(Node** head, string value) {
     newNode->data = value;
     newNode->next = *head;
     *head = newNode;
-    
+
     cout << "[LOG] Node added with value: " << value << "on the address: " << newNode << endl;
 }
 
 void displayList(Node* head) {
     cout << "\n--- MEMORY MAP LIST ---\n";
     Node* current = head;
+    
+    while (current != nullptr) {
+        cout << "Valor: " << current->data 
+             << " | Endereço Atual: " << current 
+             << " | Aponta para: " << current->next << endl;
+        
+        current = current->next;
+    }
+    cout << "--------------------------------\n" << endl;
 }
 
 void freeMemory(Node* head) {
     Node* current = head;
     while (current != nullptr) {
-        Node* nextNode = current -> next;
+        Node* nextNode = current->next;
+        
         cout << "[DELETING] Freeing memory at address: " << current << endl;
+        
+        delete current;
+        
         current = nextNode;
     }
 }
 
-int main(){
+int main() {
     Node* listHead = nullptr;
-    
-    cout << "Welcome to the Memory Map List!\n" << endl;
 
-    addNode(&listHead, "Node 1");
-    addNode(&listHead, "Node 2");
-    addNode(&listHead, "Node 3");
+    cout << "Welcome to the Memory Map List!\n" << endl;
+    cout << "=================================" << endl;
     
+    addNode(&listHead, "Python_Script");
+    addNode(&listHead, "Docker_Config");
+    addNode(&listHead, "PostgreSQL_DB");
+
     displayList(listHead);
-    
+
     freeMemory(listHead);
-    
+
     return 0;
 }
